c9c02b3356387f5d692370092fd1fedb36363a53
vanafroo <Vanafroo@Vanas-MacBook-Pro.local>
2016-04-07 15:44:09 +0700

    no message

--- Week3/js/shop.js
+++ Week3/js/shop.js
@@ -76,6 +76,23 @@ var calendar = {
 }
 calendar.displayCalendar();
 
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greet;
+
+if (hrs < 12)
+    this.greet = 'Good Morning';
+else if (hrs >= 12 && hrs <= 17)
+    this.greet = 'Good Afternoon';
+else if (hrs >= 17 && hrs <= 24)
+    this.greet = 'Good Evening';
+
+
+document.getElementById("hello").innerHTML =
+    "" + greet + ",";
+
+
 // var products = [
 //     "Brooklyn T-Shirt White",
 //     "Brooklyn T-Shirt Black",
