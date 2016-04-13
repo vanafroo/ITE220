de1ccbe2f4148444f95eea18dfab0f9a768e3b42
vanafroo <Vanafroo@Vanas-MacBook-Pro.local>
2016-04-13 13:54:21 +0700

    no message

--- Week3-2/js/task1.js
+++ Week3-2/js/task1.js
@@ -1,33 +1,29 @@
-(function() {
-    var task1 = {
-        render: function() {
-            document.getElementById("screenX").textContent = window.screenX;
-            document.getElementById("screenY").textContent = window.screenY;
+(function(){
+var task1 = {
+    render: function() {
+        document.getElementById("screenX").textContent = window.screenX;
+        document.getElementById("screenY").textContent = window.screenY;
 
-            document.getElementById("screenWidth").textContent = window.innerWidth;
-            document.getElementById("screenHeight").textContent = window.innerHeight;
+        document.getElementById("screenWidth").textContent = window.innerWidth;
+        document.getElementById("screenHeight").textContent = window.innerHeight;
 
-            document.getElementById("location").textContent = window.location.pathname;
+        document.getElementById("location").textContent = window.location.pathname;
 
 
-        },
+    },
 
-        resize: function() {
-            window.addEventListener("resize", this.render);
-        },
-        devInfo: function() {
-            var btnDev = document.getElementById("dev-info");
-            btnDev.addEventListener("click", function() {
-
-                document.getElementById("info-dev").innerHTML = 'Vana Lobato </br>IT Student </br> <a href="file:///Users/Vanafroo/Desktop/GitHub%20ITE220/Week3-2/task1.html">file:///Users/Vanafroo/Desktop/GitHub%20ITE220/Week3-2/task1.html</br>';
-            });
-        },
-       window: function() {
-   window.open("file:///Users/Vanafroo/Desktop/GitHub%20ITE220/Week3-2/task1.html", '_blank');
-}
+    resize: function() {
+        window.addEventListener("resize", this.render);
+    },
+    devInfo: function(){
+        var btnDev = document.getElementById("dev-info");
+        btnDev.addEventListener("click", function(){
+          document.getElementById("info-dev").innerHTML = 'Vana Lobato </br>IT Student </br> <a href="www.github.com/14110017">www.github.com/14110017</br>';  
+        });
     }
-    task1.render();
-    task1.resize();
-    task1.devInfo();
+}
+task1.render();
+task1.resize();
+task1.devInfo();
 
 })();
